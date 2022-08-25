6397612e910d3cc22b9937578f26299bf66ac801
Nilson Max <nilsonbenitez@hotmail.com>
2022-08-25 02:39:26 -0500

    deploy 24-08-2022 hora 2:39

---
+++ index.js
@@ -0,0 +1,15 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { allData } = require("./src/bulkCreate.js");
+
+conn.sync({ force: true }).then(() => {
+  try {
+    server.listen(process.env.PORT, async () => {
+      await allData();
+      console.log("Data loaded");
+      console.log(`%s listening at ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
